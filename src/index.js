8926922a360ba799cea48d9f2de40940d13de2e2
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-29 17:58:23 -0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+
 // import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkIBGID1zRhWxdbAbnkV3-Q2mB2QybDfQ",
+    authDomain: "projects-page-65b11.firebaseapp.com",
+    databaseURL: "https://projects-page-65b11.firebaseio.com",
+    projectId: "projects-page-65b11",
+    storageBucket: "projects-page-65b11.appspot.com",
+    messagingSenderId: "898372922382",
+    appId: "1:898372922382:web:a6dca94cf247a68807605f",
+    measurementId: "G-K7L0K8SZZ2"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
