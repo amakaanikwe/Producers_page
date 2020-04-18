ca2d896c95cdef88a53cdfb9d9bb0fd52fed5e12
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-17 23:58:53 -0400

    Updated FooterComp

--- src/components/FooterComp.jsx
+++ src/components/FooterComp.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
+import React from "react";
 
-class FooterComp extends React.Component{
-  
-  
-
-  render () {
-
+class FooterComp extends React.Component {
+  render() {
     return (
-            <footer>
-                <div className="panel-footer text-center">
-                    <a href={this.props.data.git}>
-                        <button class="btn btn-lg">
-                            <i className="fab fa-github" aria-hidden="true"></i>
-                        </button>
-                    </a>
-                    <a href={this.props.data.site}>
-                        <button className="btn btn-lg">
-                            <i className="fa fa-globe" aria-hidden="true"></i>
-                        </button>
-                    </a>
-                </div>
-            </footer>
-
+      <footer>
+        <div className="panel-footer text-center">
+          <a href={this.props.data.git}>
+            <button class="btn btn-lg">
+              <i className="fab fa-github" aria-hidden="true"></i>
+            </button>
+          </a>
+          <a href={this.props.data.site}>
+            <button className="btn btn-lg">
+              <i className="fa fa-globe" aria-hidden="true"></i>
+            </button>
+          </a>
+        </div>
+      </footer>
     );
   }
 }
