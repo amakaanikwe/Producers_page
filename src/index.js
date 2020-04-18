1a49f2b3f3425d0255811736e03781a61f454686
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-17 23:57:59 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as firebase from 'firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as firebase from "firebase";
 
 // import * as serviceWorker from './serviceWorker';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBkIBGID1zRhWxdbAbnkV3-Q2mB2QybDfQ",
-    authDomain: "projects-page-65b11.firebaseapp.com",
-    databaseURL: "https://projects-page-65b11.firebaseio.com",
-    projectId: "projects-page-65b11",
-    storageBucket: "projects-page-65b11.appspot.com",
-    messagingSenderId: "898372922382",
-    appId: "1:898372922382:web:a6dca94cf247a68807605f",
-    measurementId: "G-K7L0K8SZZ2"
-  };
+  apiKey: "AIzaSyBkIBGID1zRhWxdbAbnkV3-Q2mB2QybDfQ",
+  authDomain: "projects-page-65b11.firebaseapp.com",
+  databaseURL: "https://projects-page-65b11.firebaseio.com",
+  projectId: "projects-page-65b11",
+  storageBucket: "projects-page-65b11.appspot.com",
+  messagingSenderId: "898372922382",
+  appId: "1:898372922382:web:a6dca94cf247a68807605f",
+  measurementId: "G-K7L0K8SZZ2",
+};
 
 firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
