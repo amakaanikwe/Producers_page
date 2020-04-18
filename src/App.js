f895013f13dcf4abefe15f7d19e9db0f3ae6e98d
Amaka Anikwe <amakaanikwe@gmail.com>
2020-04-17 23:57:37 -0400

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import ProjectComp from './components/ProjectComp.jsx'
-import data from './project_page_data.json'
-import './App.css';
+import React from "react";
+import ProjectComp from "./components/ProjectComp.jsx";
+import data from "./project_page_data.json";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        data: data,
-        
-      }
-
+    this.state = {
+      data: data,
+    };
   }
-  render () {
+  render() {
     return (
       <div>
         <header>Projects Page</header>
-        {this.state.data.map((producers, i) => <ProjectComp key = {i} producers = {producers} data={this.state} />)}
+        {this.state.data.map((producers, i) => (
+          <ProjectComp key={i} producers={producers} data={this.state} />
+        ))}
 
         <div className="container">
           <div className="row">
             <div className="col-lg-12 text-center">
-              <a className="backButton" href="https://amakaanikwe.github.io/" type="button" target="_blank">Back to Site</a>
+              <a
+                className="backButton"
+                href="https://amakaanikwe.github.io/"
+                type="button"
+                target="_blank"
+              >
+                Back to Site
+              </a>
               {/* <a className="backButton" href="https://amakaanikwe.github.io/" type="button" target="_blank">Download Resume</a> */}
             </div>
           </div>
         </div>
-
       </div>
     );
   }
